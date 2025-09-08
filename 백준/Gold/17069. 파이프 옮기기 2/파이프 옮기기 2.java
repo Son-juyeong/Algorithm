57e51a468ada2d2b308035c7d597a24e9867fc1a
Son-juyeong <102614018+Son-juyeong@users.noreply.github.com>
2025-09-08 23:05:05 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 108 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+class Main {
+
+    static char[][] arr;
+
+    // 0: 가로 1: 세로 2: 대각선
+    static long[][][] dp;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        arr = new char[N + 1][N + 1];
+        dp = new long[N + 1][N + 1][3];
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = st.nextToken().charAt(0);
+            }
+        }
+
+        dp[1][2][0] = 1;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 3; j <= N; j++) {
+                if (arr[i][j] == '0') {
+                    // 현 위치 파이프 모양이 가로일 때
+                    dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+
+                    // 현 위치 파이프 모양이 세로일 때
+                    dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+
+                    if (arr[i - 1][j] == '1' || arr[i][j - 1] == '1') continue;
+                    // 현 위치 파이프 모양이 대각선일 때
+                    dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+                }
+            }
+        }
+
+        long answer = dp[N][N][0] + dp[N][N][1] + dp[N][N][2];
+        System.out.println(answer);
+    }
+}
