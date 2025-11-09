e190cdc5feb401479a124e2f2a297e456c063478
Son-juyeong <102614018+Son-juyeong@users.noreply.github.com>
2025-11-09 17:38:39 +0900

    [Bronze V] Title: 아!, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        System.out.println((str1.length()>=str2.length())?"go":"no");
+    }
+}
