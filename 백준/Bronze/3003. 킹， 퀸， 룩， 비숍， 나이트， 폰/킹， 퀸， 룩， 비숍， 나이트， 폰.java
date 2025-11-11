414e880dae8ddb4d8ea5a24f5d8f086d65c7bedf
Son-juyeong <102614018+Son-juyeong@users.noreply.github.com>
2025-11-11 23:35:59 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        int[] arr = {1, 1, 2, 2, 2, 8};
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0;i<6;i++){
+            int n = Integer.parseInt(st.nextToken());
+            sb.append(arr[i] - n).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
