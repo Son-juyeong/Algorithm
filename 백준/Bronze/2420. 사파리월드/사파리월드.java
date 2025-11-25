b99bc487873de0157f733121765f4ca995888324
Son-juyeong <102614018+Son-juyeong@users.noreply.github.com>
2025-11-25 23:29:31 +0900

    [Bronze V] Title: 사파리월드, Time: 64 ms, Memory: 11448 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long answer = Long.parseLong(st.nextToken());
+        answer -= Long.parseLong(st.nextToken());
+        System.out.println(Math.abs(answer));
+    }
+}
