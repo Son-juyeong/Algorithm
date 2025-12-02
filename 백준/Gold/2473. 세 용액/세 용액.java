cf38f77e9ec0ac4625aac9e22ff3cc20ed184bc2
Son-juyeong <102614018+Son-juyeong@users.noreply.github.com>
2025-12-02 21:22:22 +0900

    [Gold III] Title: 세 용액, Time: 152 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -18,26 +18,22 @@ public class Main {
         long[] answer = new long[3];
 
         for (int i = 0; i < N - 2; i++) {
-            for (int j = i + 2; j < N; j++) {
-                int left = i, right = j;
-                while (left < right) {
-                    int middle = (left + right) / 2;
-                    if (middle == i || middle == j) break;
-                    long temp = arr[i] + arr[middle] + arr[j];
-                    if (Math.abs(temp) < Math.abs(best)) {
-                        best = temp;
-                        answer[0] = arr[i];
-                        answer[1] = arr[middle];
-                        answer[2] = arr[j];
-                    }
-                    if (temp < 0) left = middle + 1;
-                    else if (temp > 0) right = middle;
-                    else break;
+            int left = i + 1, right = N - 1;
+            while (left < right) {
+                long temp = arr[i] + arr[left] + arr[right];
+                if (Math.abs(temp) < Math.abs(best)) {
+                    best = temp;
+                    answer[0] = arr[i];
+                    answer[1] = arr[left];
+                    answer[2] = arr[right];
                 }
                 if (best == 0) break;
+                if (temp < 0) ++left;
+                else if (temp > 0) --right;
             }
             if (best == 0) break;
         }
+
         for (int i = 0; i < 3; i++) {
             System.out.print(answer[i] + " ");
         }
