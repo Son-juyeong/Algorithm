cf46e2b069eda56dd97299626bf72ad3c9a81a7f
Son-juyeong <102614018+Son-juyeong@users.noreply.github.com>
2025-12-10 23:24:21 +0900

    [Gold IV] Title: 팰린드롬?, Time: 712 ms, Memory: 242668 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -6,16 +6,6 @@ public class Main {
     static int[][] palindrome;
     static int[] arr;
 
-    public static int isPalindrome(int start, int end){
-        while(start<=end){
-            if(arr[start]!=arr[end])
-                return 0;
-            ++start;
-            --end;
-        }
-        return 1;
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -24,19 +14,28 @@ public class Main {
         arr = new int[n + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 1;i<=n;i++){
+        for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 1;i<=n;i++){
-            for(int j = i;j<=n;j++){
-                palindrome[i][j] = isPalindrome(i, j);
+        for (int i = 1; i <= n; i++) {
+            palindrome[i][i] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == arr[i + 1])
+                palindrome[i][i + 1] = 1;
+        }
+        for (int cnt = 3; cnt <= n; cnt++) {
+            for (int s = 1; s <= n - cnt + 1; s++) {
+                if (arr[s] == arr[s + cnt - 1] && palindrome[s + 1][s + cnt - 2] == 1) {
+                    palindrome[s][s + cnt - 1] = 1;
+                }
             }
         }
 
         int m = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for(int i = 0;i<m;i++){
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
