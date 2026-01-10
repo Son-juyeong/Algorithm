d8c93d6fc6d5067fc1807fde2bf8579135e2304c
Son-juyeong <102614018+Son-juyeong@users.noreply.github.com>
2026-01-10 23:29:53 +0900

    [Bronze V] Title: 등록, Time: 56 ms, Memory: 11192 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(463);
+        System.out.println("mongdd31");
+    }
+}
